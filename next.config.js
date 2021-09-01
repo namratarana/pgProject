8dcacb1171080db7e06a30c7027216116d38adca
Namrata <namratarana9@gmail.com>
2021-09-01 18:22:03 +0530

    export removed

--- next.config.js
+++ next.config.js
@@ -16,3 +16,22 @@ module.exports = {
       ]
     },
   }
+  module.exports = {
+    trailingSlash: true,
+  }
+
+  module.exports = {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        '/PGPro-SmartAssist-Home': { page: '/PGPro-SmartAssist-Home' },
+        '/PGPro-SmartAssist-Quiz': { page: '/PGPro-SmartAssist-Quiz' },
+        '/PGPro-SmartAssist-Recommendations': { page: '/PGPro-SmartAssist-Recommendations' },
+
+       
+      }
+    },
+  }
