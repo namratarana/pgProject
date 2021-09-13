f959c10b2afdae9d4c2cc0510f67b9aa9e51038f
Namrata <namratarana9@gmail.com>
2021-09-13 11:02:54 +0530

    errorPage updated

--- pages/_error.js
+++ pages/_error.js
@@ -1,8 +1,48 @@
-import ErrorPage from "../components/ErrorPage"
-function Error({ statusCode }) {
-    return (
-      <ErrorPage/>
-  )
+import ErrorPage from "../components/ErrorPage";
+import {useLocation} from 'react-router-dom';
+import router, {useRouter} from 'next/router';
+import Quiz from './PGPro-SmartAssist-Quiz';
+import Recommendations from './PGPro-SmartAssist-Recommendations';
+
+
+function Error({ statusCode, pathname }) {
+
+    // console.log('pathname:', pathname);
+    //let location = useLocation();
+    let Router = useRouter();
+    console.log('router:', Router.asPath);
+    //console.log('location:', location);
+// ]console.log('locationPath:', location.pathname);
+    if(Router.asPath === '/PGPro-SmartAssist-Quiz')
+    {
+      Router.push('/PGPro-SmartAssist-Quiz')
+      return true;
+    }
+    else if(Router.asPath === '/PGPro-SmartAssist-Recommendations')
+    {
+      Router.push('/PGPro-SmartAssist-Recommendations')
+      return true;
+    }
+    else if(Router.asPath === '/')
+    {
+      Router.push('/')
+      return true;
+    }
+    else
+    {
+      console.log("asPath:", Router.asPath);
+      if(Router.asPath !="/" && Router.asPath !="PGPro-SmartAssist-Quiz" && Router.asPath !="/PGPro-SmartAssist-Recommendations")
+      {
+        return(
+          <ErrorPage/>
+        )
+      }
+      else
+      {
+        return true;
+      }
+      
+    }
 }
 
 Error.getInitialProps = ({ res, err }) => {
